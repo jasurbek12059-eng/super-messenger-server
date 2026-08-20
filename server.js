c5ddae968981615aa5027c3ed413680361ab1b71
jasurbek12059-eng <jasurbek12059@gmail.com>
2026-08-20 19:03:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Super Messenger Server ishlayapti!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server ${PORT} portda ishga tushdi`);
+});
